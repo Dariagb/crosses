594cdf5ba7873f214f18d424af73084c217107da
Darya Belousova <136568577+Dariagb@users.noreply.github.com>
2024-04-17 15:24:56 +0300

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+ int theNumber = (int)(Math.random()*100+1);
+
+            System.out.println(theNumber);
+        System.out.println("Введи число от 0 до 100");
+        Scanner scanner=new Scanner(System.in);
+        int guess= scanner.nextInt();
+           ;
+        }
+    }
